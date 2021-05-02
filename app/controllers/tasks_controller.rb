eceab959a64a47571a1675dc5043bbfc8cd410d1
emi.nakata <19bn022h@rikkyo.ac.jp>
2021-05-02 06:27:05 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+    
+     before_action :set_task, only: [:show, :edit, :update, :destroy]
    
     def index
         @tasks=Task.all
@@ -21,15 +23,13 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task=Task.find(params[:id])
     end
     
     def show
-        @task=Task.find(params[:id])
     end
     
     def update
-        @task=Task.find(params[:id])
+       
         
         if @task.update(task_params)
             flash[:success]='タスクが正常に更新されました'
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task=Task.find(params[:id])
+
         @task.destroy
         
         flash[:success]='タスクが正常に削除されました'
@@ -49,6 +49,11 @@ class TasksController < ApplicationController
     end
     
     private
+    
+    def set_task
+         @task=Task.find(params[:id])
+    end
+    
     def task_params
         params.require(:task).permit(:content)
     end
